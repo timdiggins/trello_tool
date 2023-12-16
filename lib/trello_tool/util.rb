3d81d719b7e2c177570de07b14c3f90d58444849
Tim Diggins <tim@red56.uk>
2023-12-16 09:41:03 +0000

    add a release command (trello release)

--- lib/trello_tool/util.rb
+++ lib/trello_tool/util.rb
@@ -3,11 +3,37 @@
 module TrelloTool
   # small utility methods for handling trello urls
   module Util
+    private
+
     def extract_id_from_url(trello_board_url)
       match_data = %r{https://trello.com/b/([a-zA-Z0-9]+)/?.*}.match(trello_board_url)
       raise "Unexpectedly #{trello_board_url} doesn't match https://trelloc.om/b/ID/description" unless match_data
 
       match_data[1]
+    end
+
+    # @param board [Trello::Board]
+    # @param list_name [String]
+    # @return [Trello::List]
+    def find_list_by_list_name(board, list_name)
+      list = board.lists.detect { |l| l.name == list_name }
+      return list if list
+
+      say("couldn't find list called #{list_name.inspect}. found:")
+      lists(url)
+      nil
+    end
+
+    def find_pos_before_list(board, target_list)
+      previous_list_pos = nil
+      board.lists.each do |this_list|
+        return previous_list_pos && ((target_list.pos + previous_list_pos) / 2) if this_list == target_list
+
+        previous_list_pos = this_list.pos
+      end
+
+      say("couldn't find list")
+      nil
     end
   end
 end
